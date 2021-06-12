56fe9d6e54c9693a41fdecc583f63caaedfd652a
Eric Shapiro <eshapiro42@gmail.com>
2021-06-12 13:07:56 -0700

    Make toasts less hideous

--- static/js/bootstrap-toaster.js
+++ static/js/bootstrap-toaster.js
@@ -27,10 +27,8 @@ TOAST_TEMPLATE.setAttribute("data-autohide", "false");
 TOAST_TEMPLATE.innerHTML = `
         <div class="toast-header">
             <span class="status-icon bi mr-2" aria-hidden="true"></span>
+            &nbsp;
             <strong class="mr-auto toast-title"></strong>
-            <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
         </div>
         <div class="toast-body"></div>`;
 
