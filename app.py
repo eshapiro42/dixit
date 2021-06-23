552317ed5a86f74e2adc92546bf13532d369940d
Eric Shapiro <eshapiro42@gmail.com>
2021-06-22 17:24:29 -0700

    Revert "Refactor player/cpu interactions"
    This reverts commit a94df1f396a49357b8be5b1b255b94efbb2fdd34.

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import os
 import random
 import string
+import sys
+import threading
+import time
 from pusher import Pusher
 from flask import (
     Flask,
@@ -12,7 +15,7 @@ from flask import (
     url_for,
 )
 
-from game import Game, Player
+from game import Game, State
 
 
 app = Flask(__name__)
@@ -23,6 +26,8 @@ except KeyError:
 random.seed()
 games = {}
 players = {}
+creators = {}
+messages = {}
 pusher = Pusher(app_id="982239", key="aac926d8b7731623a59a", secret="199bad4a11c6eae764d1", cluster="us3")
 
 
@@ -42,6 +47,19 @@ class AppError(Exception):
         return rv
 
 
+def playerChannel(player_name, game_id):
+    allowed_characters = string.ascii_letters + string.digits + "_-=@,.;"
+    channel_name = player_name
+    for character in player_name:
+        if character not in allowed_characters:
+            channel_name = channel_name.replace(character, "_")
+    return "dixit-{}-{}".format(channel_name, game_id)
+
+
+def gameChannel(game_id):
+    return "dixit-{}".format(game_id)
+
+
 @app.errorhandler(AppError)
 def handle_app_error(error):
     response = jsonify(error.to_dict())
@@ -58,18 +76,18 @@ def index():
     return render_template("index.html", data=data)
 
 
-@app.route("/play")
-def play():
+@app.route("/play", defaults={"rejoin": False})
+@app.route("/play/<rejoin>", methods=["GET", "POST"])
+def play(rejoin):
     print("PLAY Session data: {}".format(session))
     print("started={}".format(games[session["game_id"]].started))
-    game_id: str = session["game_id"]
-    game: Game = games[game_id]
     data = {
         "player_name": session["player_name"],
-        "game_id": game_id,
-        "creator": game.creator,
+        "game_id": session["game_id"],
+        "creator": creators["game_id"],
         "started": games[session["game_id"]].started,
         "random_number": random.randint(10000000, 99999999),
+        "rejoin": rejoin,
     }
     return render_template("play.html", data=data)
 
@@ -84,47 +102,61 @@ def createGame():
     while game_id in games:
         game_id = "".join(random.choice(characters) for i in range(4))
     # Create the game object
-    game = Game(game_id, pusher)
+    game = Game(game_id)
     # Add the game object to the global dictionary of games
     games[game_id] = game
     session["game_id"] = game_id
     player_name = str(request.form["player_name"]).strip()
     session["player_name"] = player_name
     # Add the player to the game
-    player_object = game.add_player(player_name, creator=True)
+    player_object = game.add_player(player_name)
     # Add the player object to the global dictionary of players
     players[(player_name, game_id)] = player_object
+    creators["game_id"] = player_name
     print("CREATEGAME Session data: {}".format(session))
-    game.interactions.send_message("{} created the game room.".format(player_name))
-    game.interactions.send_message("Waiting for players.")
+    messages[game_id] = ""
+    gameMessage(game_id, "{} created the game room.".format(player_name))
+    gameMessage(game_id, "Waiting for players.")
     return redirect(url_for("play"))
 
 
 @app.route("/api/joinGame", methods=["POST"])
 def joinGame():
     print("Received form: {}".format(request.form))
-    game_id: str = str(request.form["game_id"]).upper().strip()
+    game_id = str(request.form["game_id"]).upper().strip()
     player_name = str(request.form["player_name"]).strip()
     session["player_name"] = player_name
     session["game_id"] = game_id
     session.modified = True
     try:
-        game: Game = games[game_id]
+        game = games[game_id]
     except KeyError:
         raise AppError("Game ID {} was not found.".format(game_id))
-    # Create a new player object
-    player_object = game.add_player(player_name)
-    if player_object is None:
-        raise AppError("Could not add player. Game {} has already started.".format(game_id))
-    players[(player_name, game_id)] = player_object
-    game.interactions.send_message("{} has joined the game.".format(player_name))
+    # If this player is already in the game, re-add them
+    if (player_name, game_id) in players:
+        rejoin = True
+        gameMessage(game_id, "{} has rejoined the game.".format(player_name))
+    # If this player is not already in the game, create a new player object
+    else:
+        rejoin = False
+        player_object = game.add_player(player_name)
+        if player_object is None:
+            raise AppError("Could not add player. Game {} has already started.".format(game_id))
+        players[(player_name, game_id)] = player_object
+        playerJoined(game_id, player_name)
+        gameMessage(game_id, "{} has joined the game.".format(player_name))
     print("JOINGAME Session data: {}".format(session))
-    return redirect(url_for("play"))
+    if game.playable:
+        gamePlayable(game_id)
+    if rejoin:
+        return redirect(url_for("play", rejoin=True))
+    else:
+        return redirect(url_for("play"))
 
 
 @app.route("/api/addCPU", methods=["POST"])
 def addCPU():
-    game_id: str = session["game_id"]
+    game_id = session["game_id"]
     try:
         game: Game = games[game_id]
     except KeyError:
@@ -135,96 +167,264 @@ def addCPU():
     if player_object is None:
         raise AppError("Could not add player. Game {} has already started.".format(game_id))
     players[(player_name, game_id)] = player_object
-    game.interactions.send_message("{} has joined the game.".format(player_name))
+    playerJoined(game_id, player_name)
+    gameMessage(game_id, "{} has joined the game.".format(player_name))
+    if game.playable:
+        gamePlayable(game_id)
+    return ""
+
+
+@app.route("/api/rejoin", methods=["POST"])
+def rejoin():
+    game_id = session["game_id"]
+    player_name = session["player_name"]
+    game = games[game_id]
+    player = players[(player_name, game_id)]
+    data = {
+        "started": game.started,
+        "num_players": game.num_players,
+    }
+    pusher.trigger(playerChannel(player_name, game_id), "rejoin", data)
+    showHand(game_id, player_name)
+    if game.state == State.HOST_CHOOSING and player == game.host:
+        startHostTurn(game_id)
+    elif game.state == State.OTHERS_CHOOSING and player != game.host:
+        startOtherTurn(game_id)
+    elif game.state == State.VOTING and player != game.host:
+        startVoting(game_id)
+    else:
+        sendOutcomes(game_id)
     return ""
 
 
 @app.route("/api/startGame", methods=["POST"])
 def startGame():
-    game_id: str = session["game_id"]
-    game: Game = games[game_id]
+    game_id = session["game_id"]
+    game = games[game_id]
     if game.started:
         return ""
     ret = game.start()
     if ret is None:
         raise AppError("You cannot start a game with fewer than four players")
     print("Game {} started".format(game_id))
-    game.interactions.send_message("The game has started.")
+    gameMessage(game_id, "The game has started.")
+    data = {
+        "started": True,
+        "num_players": game.num_players,
+    }
+    pusher.trigger(gameChannel(game_id), "started", data)
+    if game.state == State.HOST_CHOOSING:
+        showHands(game_id)
+        startHostTurn(game_id)
     return ""
 
 
 @app.route("/api/getMessages")
 def getMessages():
-    game_id: str = session["game_id"]
-    player_name: str = session["player_name"]
-    player: Player = players[(player_name, game_id)]
-    player.interactions.send_message_history()
+    game_id = session["game_id"]
+    gameMessage(game_id, None)
     return ""
 
 
 @app.route("/api/sendHostChoice", methods=["POST"])
 def sendHostChoice():
-    game_id: str = session["game_id"]
-    player_name: str = session["player_name"]
-    player: Player = players[(player_name, game_id)]
+    game_id = session["game_id"]
+    player_name = session["player_name"]
     game: Game = games[game_id]
-    host_card: str = request.form["hostCard"]
-    host_prompt: str = request.form["hostPrompt"]
+    host_card = request.form["hostCard"]
+    host_prompt = request.form["hostPrompt"]
     print("Host {} chose card {}".format(player_name, host_card))
     game.current_round.hostChoice(host_card, host_prompt)
-    player.interactions.show_hand()
+    showHand(game_id, player_name)
     message = '''{}'s prompt: "{}"'''.format(game.host.name, game.current_round.host_prompt)
-    game.interactions.send_host_prompt(host_prompt)
-    game.interactions.send_message(message, bold=True)
-    game.loop.send(None)
+    hostPrompt(game_id, host_prompt)
+    gameMessage(game_id, message, bold=True)
+    if game.state == State.OTHERS_CHOOSING:
+        startOtherTurn(game_id)
     return ""
 
 
 @app.route("/api/sendOtherChoice", methods=["POST"])
 def sendOtherChoice():
-    game_id: str = session["game_id"]
-    player_name: str = session["player_name"]
-    player: Player = players[(player_name, game_id)]
+    game_id = session["game_id"]
+    player_name = session["player_name"]
+    player = players[(player_name, game_id)]
     game: Game = games[game_id]
-    card: str = request.form["card"]
+    card = request.form["card"]
     print("Player {} chose card {}".format(player_name, card))
     game.current_round.otherChoice(player, card)
-    player.interactions.show_hand()
+    showHand(game_id, player_name)
     message = """{} played a card.""".format(player_name)
-    game.interactions.send_message(message)
-    game.loop.send(None)
+    gameMessage(game_id, message)
+    if game.state == State.CPUS_CHOOSING:
+        for cpu_player in game.cpu_players:
+            game.current_round.cpuChoice(cpu_player)
+            message = """{} played a card.""".format(cpu_player.name)
+            gameMessage(game_id, message)
+        game.loop.send(None)
+        startVoting(game_id)
     return ""
 
 
 @app.route("/api/sendVote", methods=["POST"])
 def sendVote():
-    game_id: str = session["game_id"]
-    player_name: str = session["player_name"]
+    game_id = session["game_id"]
+    player_name = session["player_name"]
     player = players[(player_name, game_id)]
     game: Game = games[game_id]
-    vote: str = request.form["card"]
+    vote = request.form["card"]
     print("Player {} voted for card {}".format(player_name, vote))
     game.current_round.vote(player, vote)
     message = """{} voted.""".format(player_name)
-    game.interactions.send_message(message)
-    game.loop.send(None)
+    gameMessage(game_id, message)
+    if game.state == State.CPUS_VOTING:
+        for cpu_player in game.cpu_players:
+            game.current_round.cpuVote(cpu_player)
+            message = """{} voted.""".format(cpu_player.name)
+            gameMessage(game_id, message)
+        game.loop.send(None)
+        # Scoring is complete, so start the next round!
+        sendOutcomes(game_id)
+        scoreUpdate(game_id)
+        game.current_round.end()
+        startHostTurn(game_id)
     return ""
 
 
 @app.route("/api/sendMulligan", methods=["POST"])
 def sendMulligan():
-    game_id: str = session["game_id"]
-    player_name: str = session["player_name"]
-    player: Player = players[(player_name, game_id)]
-    game: Game = games[game_id]
-    card: str = request.form["card"]
+    game_id = session["game_id"]
+    player_name = session["player_name"]
+    player = players[(player_name, game_id)]
+    game = games[game_id]
+    card = request.form["card"]
     print("Player {} mulliganed card {}".format(player_name, card))
     player.discard_card(card)
     player.draw_card()
-    player.interactions.show_hand(game_id, player_name)
+    showHand(game_id, player_name)
     message = """{} used a mulligan.""".format(player_name)
-    game.interactions.send_message(message)
+    gameMessage(game_id, message)
     return ""
+
+
+def startHostTurn(game_id):
+    game = games[game_id]
+    host_name = game.host.name
+    gameMessage(game_id, "It is {}'s turn to choose a card and give a prompt.".format(host_name))
+    data = {
+        "host": host_name,
+    }
+    pusher.trigger(gameChannel(game_id), "startHostTurn", data)
+
+
+def startOtherTurn(game_id):
+    game = games[game_id]
+    host_name = game.host.name
+    message = "Other players, choose a card to match the prompt."
+    gameMessage(game_id, message)
+    data = {
+        "host": host_name,
+    }
+    pusher.trigger(gameChannel(game_id), "startOtherTurn", data)
+
+
+def startVoting(game_id):
+    game = games[game_id]
+    host_name = game.host.name
+    message = "Other players, vote for which card you think matches the prompt."
+    gameMessage(game_id, message)
+    data = {
+        "host": host_name,
+    }
+    showTable(game_id)
+    pusher.trigger(gameChannel(game_id), "startVoting", data)
+
+
+def showHand(game_id, player_name):
+    print("showing hand of player {} in game {}".format(player_name, game_id))
+    player = players[(player_name, game_id)]
+    pusher.trigger(playerChannel(player_name, game_id), "showHand", player.hand)
+
+
+def showHands(game_id):
+    game = games[game_id]
+    for player in game.players:
+        showHand(game_id, player.name)
+
+
+def showTable(game_id):
+    print("showing table in game {}".format(game_id))
+    game = games[game_id]
+    cardnum = 1
+    table_list = list(game.table.values())
+    random.shuffle(table_list)
+    pusher.trigger(gameChannel(game_id), "showTable", table_list)
+
+
+def gameMessage(game_id, gameMessage, bold=False):
+    print("Sending message to all players in game {}: {}".format(game_id, gameMessage))
+    game = games[game_id]
+    if messages[game_id] == "":
+        messages[game_id] = gameMessage
+    elif gameMessage == None:
+        pass
+    else:
+        if bold:
+            messages[game_id] += "<br><b>{}</b>".format(gameMessage)
+        else:
+            messages[game_id] += "<br>{}".format(gameMessage)
+    data = {
+        "gameMessage": messages[game_id],
+    }
+    pusher.trigger(gameChannel(game_id), "gameMessage", data)
+
+
+def hostPrompt(game_id, prompt):
+    print("Sending prompt to all players in game {}: {}".format(game_id, prompt))
+    data = {
+        "hostPrompt": prompt,
+    }
+    pusher.trigger(gameChannel(game_id), "hostPrompt", data)
+
+
+def sendOutcomes(game_id):
+    print("Sending outcomes to all players in game {}".format(game_id))
+    game = games[game_id]
+    data = {
+        "cardPlayers": game.table,
+        "cardVoters": game.votes,
+        "host": game.host.name,
+    }
+    pusher.trigger(gameChannel(game_id), "sendOutcomes", data)
+
+
+def gamePlayable(game_id):
+    print("Sending playable data to game {}".format(game_id))
+    game = games[game_id]
+    pusher.trigger(gameChannel(game_id), "gamePlayable", None)
+
+
+def playerJoined(game_id, player_name):
+    data = {
+        "playerName": player_name,
+    }
+    pusher.trigger(gameChannel(game_id), "playerJoined", data)
+
+
+def scoreUpdate(game_id):
+    print("Sending scores to all players in game {}".format(game_id))
+    game = games[game_id]
+    score_html = "<tr>"
+    for player_object in game.players:
+        score_html += "\n<td> {} </td>".format(player_object.name)
+    score_html += "\n</tr>\n<tr>"
+    for player_object in game.players:
+        score_html += "\n<td> {} </td>".format(player_object.score)
+    score_html += "\n</tr>"
+    data = {
+        "scores": score_html,
+    }
+    pusher.trigger(gameChannel(game_id), "scoreUpdate", data)
 
 
 if __name__ == "__main__":
